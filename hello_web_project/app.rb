bcaa82bae08e4ce8de1963f75e1b27d54fa584c8
kateusacova <kateusacova@gmail.com>
2022-10-03 13:42:40 +0100

    Building route block and accessing request params

--- hello_web_project/app.rb
+++ hello_web_project/app.rb
@@ -7,4 +7,16 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  get '/hello' do
+    name = params[:name]
+    return "Hello #{name}"
+  end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+
+    return "Thanks #{name}, you sent this message: #{message}"
+  end
 end
